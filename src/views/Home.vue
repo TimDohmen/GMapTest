cb79938f700f7cd8627e09e739ccddffedd89204
Tim Dohmen <dohmentim@yahoo.com>
2020-03-11 14:34:59 -0600

    da map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>My First Google Map</h1>
+    <div id="googleMap" style="width:100%;height:400px;"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  mounted() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.508742, -0.12085),
+      zoom: 5
+    };
+    var map = new google.maps.Map(
+      document.getElementById("googleMap"),
+      mapProp
+    );
   }
-}
+};
 </script>
